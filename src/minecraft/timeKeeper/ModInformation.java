35d57d2e5987b7f585e9885e2283b7225fe0c51a
Ezekiel Elin <ezekielelin@me.com>
2013-11-08 11:45:14 -0500

    Updated Forge Build to 9.11.1.916

--- src/minecraft/timeKeeper/ModInformation.java
+++ src/minecraft/timeKeeper/ModInformation.java
@@ -3,6 +3,6 @@ package timeKeeper;
 public class ModInformation {
 	public static final String ID = "EzTimeKeeper";
 	public static final String NAME = "Time Keeper";
-	public static final String VERSION = "1.0.3";
+	public static final String VERSION = "1.0.4";
 	public static final String CHANNEL = "EzTK";
 }
