ff5beff95d2af529f32f5a1f024a0c1eb2cb016d
Ezekiel Elin <ezekielelin@me.com>
2016-03-27 10:56:29 -0400

    Fixed packaging

--- java/com/ezekielelin/TimeKeeper/EzTimeKeeper.java
+++ java/com/ezekielelin/TimeKeeper/EzTimeKeeper.java
@@ -23,7 +23,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class EzTimeKeeper {
 	public static final String MODID = "EzTimeKeeper";
 	public static final String NAME = "Time Keeper";
-	public static final String VERSION = "2.1";
+	public static final String VERSION = "2.1.2";
 
 	public static final TimeZone tz = Calendar.getInstance().getTimeZone();
 	
